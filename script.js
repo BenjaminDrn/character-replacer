118dad7dfba4e1213887ce9ccf3ffeb33a89295a
Benjamin <drouinbenjamin20@gmail.com>
2022-06-10 12:01:03 +0200

    v1.1 of charactere replacer. The foncionality permit to make several charactere to replace

--- script.js
+++ script.js
@@ -12,42 +12,59 @@ const fields = [inputCharacter, inputReplacement, textContainer];
 const textResult = document.querySelector('#text-result');
 const btnCopy = document.querySelector('#copy');
 
+// value of fields
+inputCharacter.value = '<,>';
+inputReplacement.value = '&lt;,&gt;';
+textContainer.value = '<h1>Character Replacer</h1>';
+
 /**
  * On submit, replace the character and display the result
  */
 
 submitButton.addEventListener('click', (e) => {
     e.preventDefault();
-    // verify if the fields are not empty
-    isNotEmpty(inputCharacter);
-    isNotEmpty(inputReplacement);
-    isNotEmpty(textContainer);
 
-    // remove classe alert if the field is focused
     fields.forEach((field) => {
+        // remove class alert if the field is focused
         field.addEventListener('focus', () => {
             field.classList.remove('alert');
         });
+
+        // verify if the fields are not empty
+        isEmpty(field);
     });
 
-    if (isNotEmpty(inputCharacter) && isNotEmpty(inputReplacement) && isNotEmpty(textContainer)) {
-        const text = textContainer.value;
-        const character = inputCharacter.value;
-        const replacement = inputReplacement.value;
-        const result = replaceCharacter(text, character, replacement);
-        textResult.innerHTML = result;
+    // variables
+    let text = textContainer.value;
+    let character = isMultiple(inputCharacter) ? inputCharacter.value.split(',') : inputCharacter.value;
+    let replacement = isMultiple(inputReplacement) ? inputReplacement.value.split(',') : inputReplacement.value;
+
+    if (!document.querySelector('.alert')) {
+        // check if character and replacement are arrays
+        if (typeof(character) === typeof(replacement)) {
+            // check if the number of character and replacement are the same
+            if (character.length == replacement.length) {
+                for (let i = 0; i < character.length; i++) {
+                    text = replaceCharacter(text, character[i], replacement[i]);
+                }
+            } else {
+                text = replaceCharacter(text, character, replacement);
+            }
+            console.log(text);
+            textResult.textContent = text;
+        } else {
+            inputCharacter.classList.add('alert');
+            inputReplacement.classList.add('alert');
+        }
     }
 });
 
 /**
  * On click, copy the result to the clipboard
  */
-
 btnCopy.addEventListener('click', () => {
     copyToClipboard(textResult);
 });
-
-
 
 /**
  * @param {*} text 
@@ -55,7 +72,6 @@ btnCopy.addEventListener('click', () => {
  * @param {*} replacement 
  * @returns {string}
  */
-
 function replaceCharacter(text, character, replacement) {
     let regex = new RegExp(character, 'g');
     return text.replace(regex, replacement);
@@ -63,15 +79,15 @@ function replaceCharacter(text, character, replacement) {
 
 /**
  * @param {*} element 
- * @returns 
+ * @returns {boolean} 
  */
-
-function isNotEmpty(element) {
+function isEmpty(element) {
     if (element.value === '') {
         element.classList.add('alert');
         setTimeout(() => {
             element.classList.remove('alert');
         }, 5000);
+        return false;
     } else {
         element.classList.remove('alert');
         return true;
@@ -79,12 +95,24 @@ function isNotEmpty(element) {
 }
 
 /**
- * Copy element content to the clipboard
+ * @param {*} element
+ * @returns {boolean}
+ */
+function isMultiple(element) {
+    let regex = new RegExp(/,/g);
+    return regex.test(element.value);
+}
+
+/**
  * @param {*} element
  */
 function copyToClipboard(element) {
     try {
         navigator.clipboard.writeText(element.textContent);
+        btnCopy.classList.add('copied');
+        setTimeout(() => {
+            btnCopy.classList.remove('copied');
+        }, 5000);
     } catch {
         console.log('Error !');
     }
